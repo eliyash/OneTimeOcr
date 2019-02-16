685e57e227f8e519db93c6a5e273b56bcb8732a1
Eliahu <eliahu.shneider@bodyvisionmedical.com>
2019-02-16 02:56:17 +0200

    save the NN in separated file

--- test_cnn.py
+++ test_cnn.py
@@ -1,36 +1,15 @@
 from __future__ import print_function
+
 import argparse
+
 import torch
-import torch.nn as nn
 import torch.nn.functional as f
-import torch.optim as optim
 from torch.utils.data import DataLoader
 from torchvision import transforms
 from torchvision.datasets import ImageFolder
-# from torchvision import datasets, transforms
+
 from paths import Locations
-
-
-class Net(nn.Module):
-    def __init__(self, num_classes):
-        super(Net, self).__init__()
-        self.conv1 = nn.Conv2d(1, 10, kernel_size=5)
-        self.conv2 = nn.Conv2d(10, 20, kernel_size=5)
-        self.conv2_drop = nn.Dropout2d()
-        self.fc1 = nn.Linear(9680, 1024)
-        self.fc2 = nn.Linear(1024, 128)
-        self.fc3 = nn.Linear(128, num_classes)
-
-    def forward(self, x):
-        x = f.relu(f.max_pool2d(self.conv1(x), 2))
-        x = f.relu(f.max_pool2d(self.conv2_drop(self.conv2(x)), 2))
-        x = x.view(-1, 9680)
-        x = f.relu(self.fc1(x))
-        x = f.dropout(x, training=self.training)
-        x = f.relu(self.fc2(x))
-        x = f.dropout(x, training=self.training)
-        x = self.fc3(x)
-        return f.log_softmax(x, dim=1)
+from mnist_like_net import Net
 
 
 def test(_, model, device, test_loader):
