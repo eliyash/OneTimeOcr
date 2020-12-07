b8e4d2debda7d4032699ee256d25f606e31cdc19
eliyash <eliyash3@gmail.com>
2020-12-07 20:40:07 +0200

    save the data for train

--- app/main.py
+++ app/main.py
@@ -52,8 +52,44 @@ class App:
         else:
             return key
 
+    def _on_save_data(self):
+        time_str = time.strftime("dataset_%Y%m%d-%H%M%S")
+        new_dataset_path = LETTERS_PATH / time_str
+
+        letters_folder = new_dataset_path / 'letters_map'
+        pages_folder = new_dataset_path / 'pages'
+        letter_images_folder = new_dataset_path / 'letter_images'
+
+        ready_map = self._data_model.is_page_ready_map
+        images_paths = self._data_model.images_paths
+        instances_locations = self._data_model.instances_locations_per_image
+        page_folder_names = [
+            self._data_model.images_paths[page].with_suffix('').name for page in range(len(instances_locations))
+        ]
+
+        for page_path, page_folder_name, instance_locations, is_ready in \
+                zip(images_paths, page_folder_names, instances_locations, ready_map):
+            page_image = cv2.imread(str(page_path))
+            self._save_pages_letters(page_image, instance_locations, letter_images_folder, is_ready)
+            self._save_page(instance_locations, pages_folder, page_folder_name, is_ready)
+
+        letters_folder.mkdir(parents=True)
+        for key, image in self._data_model.different_letters.data.items():
+            self.save_individual_images(image, key, letters_folder)
+
+    @classmethod
+    def _save_pages_letters(cls, page: np.ndarray, instances_locations: Dict, letters_folder: Path, is_ready: bool):
+        letters_folder = letters_folder if is_ready else letters_folder / 'WIP'
+        for key, duplicate_letters in instances_locations.items():
+            main_letter_folder = letters_folder / cls._key_to_str(key)
+            main_letter_folder.mkdir(parents=True, exist_ok=True)
+            for letter_location in duplicate_letters:
+                letter_image = cls._get_image_patch(page, letter_location)
+                cls.save_individual_images(letter_image, letter_location, main_letter_folder)
+
     @staticmethod
-    def _save_page(instances_locations_by_letters: Dict, path_to_save: Path):
+    def _save_page(instances_locations_by_letters: Dict, pages_folder: Path, page_folder_name: str, is_ready: bool):
+        path_to_save = (pages_folder if is_ready else pages_folder / 'WIP') / page_folder_name
         path_to_save.mkdir(parents=True)
         image_to_index = {image: str(image) for i, image in enumerate(instances_locations_by_letters.keys())}
 
@@ -64,35 +100,10 @@ class App:
         with open(str(path_to_save / 'instances_locations_by_index.json'), 'w') as fp:
             json.dump(instances_locations_by_index, fp, indent=4)
 
-    def _on_save_data(self):
-        time_str = time.strftime("dataset_%Y%m%d-%H%M%S")
-        new_dataset_path = LETTERS_PATH / time_str
-        letters_folder = new_dataset_path / 'letters_map'
-        pages_folder = new_dataset_path / 'pages'
-        letters_folder.mkdir(parents=True)
-        pages_folder.mkdir(parents=True)
-
-        images_paths = self._data_model.instances_locations_per_image
-        page_folder_names = [
-            self._data_model.images_paths[page].with_suffix('').name for page in range(len(images_paths))
-        ]
-
-        for page_folder_name, instances_locations_by_letters in zip(page_folder_names, images_paths):
-            self._save_page(instances_locations_by_letters, pages_folder / page_folder_name)
-
-        for key, image in self._data_model.different_letters.data.items():
-            self.save_individual_images(image, key, letters_folder)
-
-        # for main_letter, duplicate_letters in self._data_model.different_letters.items():
-        #     letters_folder = new_dataset_path / self._location_to_str(main_letter)
-        #     letters_folder.mkdir(parents=True, exist_ok=True)
-        #     images_of_duplicated_letters = self._get_letters_images(duplicate_letters, self._data_model.cv_image)
-        #     for letter_location, letter_image in zip(duplicate_letters, images_of_duplicated_letters):
-        #         self.save_individual_images(letter_image, letter_location, letters_folder)
-
-    def save_individual_images(self, letter_image, letter_location, letters_folder):
+    @classmethod
+    def save_individual_images(cls, letter_image, letter_location, letters_folder):
         image_as_uint = letter_image
-        file_name = letters_folder / '{}.jpg'.format(self._key_to_str(letter_location))
+        file_name = letters_folder / '{}.jpg'.format(cls._key_to_str(letter_location))
         cv2.imwrite(str(file_name), image_as_uint)
 
     def _network_detect(self):
@@ -143,11 +154,10 @@ class App:
 
     def _page_move(self, back=False):
         new_page = self._data_model.page.data + (-1 if back else 1)
-        print(new_page)
         if 0 <= new_page < self._data_model.num_of_pages:
             self._data_model.page.data = new_page
         else:
-            print('illegal page')
+            print('illegal page: {}'.format(new_page))
 
     def run(self):
         self._gui.run()
